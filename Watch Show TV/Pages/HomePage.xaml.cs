fad2efd628e25268594d816d4de11af78ede63a3
alejandro <alejandro.cayssials@gmail.com>
2018-10-31 18:47:37 -0300

    initial commit

---
+++ Watch Show TV/Pages/HomePage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TMDbLib.Objects.Search;
+using TMDbLib.Objects.TvShows;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Watch_Show_TV.Pages
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+
+            TvShow show = MainPage.client.GetLatestTvShowAsync().Result;
+
+            TvShow.Text = show.Name;
+
+            Uri uri = new Uri("https://image.tmdb.org/t/p/original" + show.PosterPath);
+
+            BitmapImage image = new BitmapImage(uri);
+
+            Image imageShow = new Image();
+            imageShow.Source = image;
+            imageShow.Height = 400;
+
+            sPanel1.Children.Add(imageShow);
+            
+
+            TextBlock txtUri = new TextBlock();
+            txtUri.Text = show.PosterPath;
+
+            sPanel1.Children.Add(txtUri);
+        }
+    }
+}
---
+++ Watch Show TV/Pages/MainPage.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TMDbLib.Client;
+using TMDbLib.Objects.Movies;
+using TMDbLib.Objects.Search;
+using TMDbLib.Objects.TvShows;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Watch_Show_TV.Pages
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static TMDbClient client;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+             client = new TMDbClient("7d637f29355aba225bd62d7af21d38d5");
+
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // you can also add items in code behind
+            /*NavView.MenuItems.Add(new NavigationViewItemSeparator());
+            NavView.MenuItems.Add(new NavigationViewItem()
+            { Content = "My content", Icon = new SymbolIcon(Symbol.), Tag = "content" });
+            */
+
+            // set the initial SelectedItem 
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "Home")
+                {
+                    NavView.SelectedItem = item;
+                    contentFrame.Navigate(typeof(HomePage));
+                    break;
+                }
+            }
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                contentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                // find NavigationViewItem with Content that equals InvokedItem
+                var item = sender.MenuItems.OfType<NavigationViewItem>().First(x => (string)x.Content == (string)args.InvokedItem);
+                NavView_Navigate(item as NavigationViewItem);
+            }
+        }
+
+        private void NavView_Navigate(NavigationViewItem item)
+        {
+            switch (item.Tag)
+            {
+                case "Home":
+                    contentFrame.Navigate(typeof(HomePage));
+                    break;
+
+                case "Favs":
+                    contentFrame.Navigate(typeof(Favs));
+                    break;
+
+                case "Calendar":
+                    contentFrame.Navigate(typeof(calendar));
+                    break;
+
+                case "News":
+                    contentFrame.Navigate(typeof(News));
+                    break;
+
+                
+            }
+        }
+
+        private void NavView_BackRequested(NavigationView sender,
+                                   NavigationViewBackRequestedEventArgs args)
+        {
+            On_BackRequested();            
+        }
+
+        private void BackInvoked(KeyboardAccelerator sender,
+                                 KeyboardAcceleratorInvokedEventArgs args)
+        {
+            On_BackRequested();
+            args.Handled = true;
+        }
+
+        private bool On_BackRequested()
+        {
+            if (!contentFrame.CanGoBack)
+                return false;
+
+            // Don't go back if the nav pane is overlayed.
+            if (NavView.IsPaneOpen &&
+                (NavView.DisplayMode == NavigationViewDisplayMode.Compact ||
+                 NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+                return false;
+
+            contentFrame.GoBack();
+            return true;
+        }
+    }
+}
